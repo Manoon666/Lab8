652243f893440aa8012c73a2320fcf48b74c6220
Manoon666 <manoon8412@gmail.com>
2024-12-06 15:56:52 +0700

    lab8_3fished

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,4 +1,50 @@
-/*
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age, height, bty;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100){ 
+            character = "Chopper";
+        } 
+        
+        else if(height >= 100 && height < 180){
+            character = "Usopp";
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bty;
+            if(bty <= 1100000000){
+                character = "Sanji";
+            }
+            else{
+                character = "Zoro";
+            }
+        }
+    }
+    else{
+        if(age <= 60){            
+            cout << "Enter your bounty: ";
+            cin >> bty;
+            if(bty <= 500000000){
+                character = "Franky";
+            }
+            else{
+                character = "Jinbe";
+            }
+        }
+        else{character = "Brook";}
+    }
+    cout << "Your character = " << character;
+    
+    return 0;
+}/*
 "Enter your age: "
 "Enter your height: "
 "Enter your bounty: "
